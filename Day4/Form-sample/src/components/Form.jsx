2cdfda0a358cde0ca75bc8bf1bdb37c68e3c8a31
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-05-13 11:36:53 +0530

    updation

--- Day4/Form-sample/src/components/Form.jsx
+++ Day4/Form-sample/src/components/Form.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Form() {
-    const submit = (event) => {};
+    // state declaration
+    const [field, setField] = useState({
+        firstName: "",
+        email: "",
+        gender: "",
+        skills: [],
+        country: "",
+    });
+
+    // function definition
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+    const handleChange = (event) => {
+        if (event.target.name === "skills") {
+            // setField({ ...field, ...field.skills, [event.target.name]: [event.target.name].push(event.target.value) });
+            setField({ [event.target.name]: [...field.skills, event.target.value] });
+            // console.log(event);
+        }
+        setField({ ...field, [event.target.name]: event.target.value });
+    };
+    console.log(field);
 
     return (
         <div>
-            <div className="container">
+            <form onSubmit={handleSubmit} className="container">
                 <h2>Signup Form</h2>
                 <div className="form-body">
                     <div className="form-row">
@@ -13,7 +34,7 @@ export function Form() {
                             <label>First Name</label>
                         </div>
                         <div className="form-value value1">
-                            <input type="text" name="firstName" />
+                            <input type="text" name="firstName" onChange={handleChange} />
                         </div>
                     </div>
 
@@ -22,7 +43,7 @@ export function Form() {
                             <label>Email</label>
                         </div>
                         <div className="form-value value1">
-                            <input type="text" name="email" />
+                            <input type="text" name="email" onChange={handleChange} />
                         </div>
                     </div>
 
@@ -31,9 +52,9 @@ export function Form() {
                             <label>Gender</label>
                         </div>
                         <div className="form-value">
-                            <input type="radio" id="male" value="male" name="gender" />
+                            <input type="radio" id="male" value="male" name="gender" onChange={handleChange} />
                             <label htmlFor="male"> Male</label>
-                            <input type="radio" id="female" value="female" name="gender" />
+                            <input type="radio" id="female" value="female" name="gender" onChange={handleChange} />
                             <label htmlFor="female">Female </label>
                         </div>
                     </div>
@@ -45,21 +66,27 @@ export function Form() {
                         <div className="form-value skills-value">
                             <div className="skills-col">
                                 <div>
-                                    <input type="checkbox" id="html" name="skills" value="html" />
+                                    <input type="checkbox" id="html" name="skills" value="html" onChange={handleChange} />
                                     <label htmlFor="html">HTML </label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" id="css" name="skills" value="css" />
+                                    <input type="checkbox" id="css" name="skills" value="css" onChange={handleChange} />
                                     <label htmlFor="css">CSS </label>
                                 </div>
                             </div>
                             <div className="skills-col">
                                 <div>
-                                    <input type="checkbox" id="javascript" name="skills" value="javascript" />
+                                    <input
+                                        type="checkbox"
+                                        id="javascript"
+                                        name="skills"
+                                        value="javascript"
+                                        onChange={handleChange}
+                                    />
                                     <label htmlFor="javascript">Javascript </label>
                                 </div>
                                 <div>
-                                    <input type="checkbox" id="php" name="skills" value="php" />
+                                    <input type="checkbox" id="php" name="skills" value="php" onChange={handleChange} />
                                     <label htmlFor="php">PHP </label>
                                 </div>
                             </div>
@@ -71,7 +98,7 @@ export function Form() {
                             <label htmlFor="">Country</label>
                         </div>
                         <div className="form-value country-value">
-                            <select name="country" id="country">
+                            <select name="country" id="country" onChange={handleChange}>
                                 <option value="">select</option>
                                 <option value="india">India</option>
                                 <option value="uae">UAE</option>
@@ -83,7 +110,7 @@ export function Form() {
                         <button>Signup</button>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
