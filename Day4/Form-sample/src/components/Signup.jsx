42d84bb5d28b3fbda00309f4256f28e7ced18cdd
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-05-22 15:41:59 +0530

    validation included onChange and onSubmit

--- Day4/Form-sample/src/components/Signup.jsx
+++ Day4/Form-sample/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 export function Signup({ closeButton }) {
-    // state declaration
+    // ************************************state declaration************************************
     const [field, setField] = useState({
         firstName: "",
         email: "",
@@ -10,26 +10,94 @@ export function Signup({ closeButton }) {
         country: "",
         password: "",
     });
+    const [errorField, setErrorField] = useState({
+        firstName: false,
+        email: false,
+        gender: false,
+        skills: false,
+        country: false,
+        password: false,
+    });
 
-    // function definition
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
+    //                                       function definition
+    //for handling input fields (onChange)
     const handleChange = (event) => {
         if (event.target.name === "skills") {
-            console.log("skills selected");
-            setField({ ...field, [event.target.name]: [...field.skills, event.target.value] });
+            let newSkills = field.skills; //doubt?????? why assiging 'field.skills'
+            if (event.target.checked) {
+                newSkills.push(event.target.value); //to add element when checked
+            } else {
+                newSkills.splice(field.skills.indexOf(event.target.value), 1); //to  remove when un-checked
+            }
+            setField((prev) => ({ ...prev, [event.target.name]: newSkills }));
             return;
         }
-        setField({ ...field, [event.target.name]: event.target.value });
+        setField((prev) => ({ ...prev, [event.target.name]: event.target.value }));
     };
     console.log(field);
 
-    const closeForm = (event) => {
-        // console.log(closeButton);
-        closeButton(false); //toggle btn
+    //for handling form submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (isFormValidOnSubmit(event)) {
+            console.log("form is Valid");
+            return;
+        }
+        console.log("form is  Invalid");
     };
-    // console.log(props.closeButton.newUser);
+
+    //               form validation.......        Basic solution
+    // const isFormValid = () => {
+    //     if (field.firstName === "") {
+    //         setErrorField((prev) => ({
+    //             ...prev,
+    //             firstName: true,
+    //         }));
+    //         return false;
+    //     } else {
+    //         setErrorField((prev) => ({
+    //             ...prev,
+    //             firstName: false,
+    //         }));
+    //         return true;
+    //     }
+    // };
+
+    /*               form validation.......        Best solution...... */
+    const isFormValidOnSubmit = (event) => {
+        let error = {
+            firstName: false,
+            email: false,
+            gender: false,
+            skills: false,
+            country: false,
+            password: false,
+        };
+        if (field.firstName === "") {
+            error.firstName = true;
+        }
+        if (field.email === "") {
+            error.email = true;
+        }
+        if (field.gender === "") {
+            error.gender = true;
+        }
+        if (field.skills !== event.target.checked) {
+            error.skills = true;
+        }
+        if (field.country === "") {
+            error.country = true;
+        }
+        if (field.password === "") {
+            error.password = true;
+        }
+        setErrorField(error);
+    };
+
+    //form closing btn handling. Update the state in the App.jsx
+    const closeForm = (event) => {
+        closeButton(false);
+    };
 
     return (
         <div>
@@ -41,94 +109,110 @@ export function Signup({ closeButton }) {
                 </div>
                 <h2>Signup Form</h2>
                 <div className="form-body">
-                    <div className="form-row">
+                    <div className="form-row" /* firstName */>
                         <div className="form-key">
                             <label htmlFor="firstName">First Name</label>
                         </div>
                         <div className="form-value value1">
                             <input type="text" id="firstName" name="firstName" onChange={handleChange} />
+                            <div className="error-field">{errorField.firstName && <p>First name is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /* Email*/>
                         <div className="form-key">
                             <label htmlFor="email">Email</label>
                         </div>
                         <div className="form-value value1">
                             <input type="text" id="email" name="email" onChange={handleChange} />
+                            <div className="error-field">{errorField.email && <p>Email is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /*Gender */>
                         <div className="form-key">
                             <label>Gender</label>
                         </div>
                         <div className="form-value">
-                            <input type="radio" id="male" value="male" name="gender" onChange={handleChange} />
-                            <label htmlFor="male"> Male</label>
-                            <input type="radio" id="female" value="female" name="gender" onChange={handleChange} />
-                            <label htmlFor="female">Female </label>
+                            <div>
+                                <input type="radio" id="male" value="male" name="gender" onChange={handleChange} />
+                                <label htmlFor="male"> Male</label>
+                                <input type="radio" id="female" value="female" name="gender" onChange={handleChange} />
+                                <label htmlFor="female">Female </label>
+                            </div>
+                            <div className="error-field">{errorField.gender && <p>Gender is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /*skills*/>
                         <div className="form-key">
                             <label>Skills</label>
                         </div>
-                        <div className="form-value skills-value">
-                            <div className="skills-col">
-                                <div>
-                                    <input type="checkbox" id="html" name="skills" value="html" onChange={handleChange} />
-                                    <label htmlFor="html">HTML </label>
+                        <div className="form-value">
+                            <div className="skills-value">
+                                <div className="skills-col">
+                                    <div>
+                                        <input
+                                            type="checkbox"
+                                            id="html"
+                                            name="skills"
+                                            value="html"
+                                            onChange={handleChange}
+                                        />
+                                        <label htmlFor="html">HTML </label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" id="css" name="skills" value="css" onChange={handleChange} />
+                                        <label htmlFor="css">CSS </label>
+                                    </div>
                                 </div>
-                                <div>
-                                    <input type="checkbox" id="css" name="skills" value="css" onChange={handleChange} />
-                                    <label htmlFor="css">CSS </label>
+                                <div className="skills-col">
+                                    <div>
+                                        <input
+                                            type="checkbox"
+                                            id="javascript"
+                                            name="skills"
+                                            value="javascript"
+                                            onChange={handleChange}
+                                        />
+                                        <label htmlFor="javascript">Javascript </label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" id="php" name="skills" value="php" onChange={handleChange} />
+                                        <label htmlFor="php">PHP </label>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="skills-col">
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        id="javascript"
-                                        name="skills"
-                                        value="javascript"
-                                        onChange={handleChange}
-                                    />
-                                    <label htmlFor="javascript">Javascript </label>
-                                </div>
-                                <div>
-                                    <input type="checkbox" id="php" name="skills" value="php" onChange={handleChange} />
-                                    <label htmlFor="php">PHP </label>
-                                </div>
-                            </div>
+                            <div className="error-field">{errorField.skills && <p>Skills is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /* country */>
                         <div className="form-key">
                             <label htmlFor="">Country</label>
                         </div>
                         <div className="form-value country-value">
                             <select name="country" id="country" onChange={handleChange}>
-                                <option value="">select</option>
+                                <option value="select">select</option>
                                 <option value="india">India</option>
                                 <option value="uae">UAE</option>
                                 <option value="canada">canada</option>
                             </select>
+                            <div className="error-field">{errorField.country && <p>Country is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /* password */>
                         <div className="form-key">
                             <label htmlFor="password">password</label>
                         </div>
                         <div className="form-value value1">
                             <input type="password" id="password" name="password" onChange={handleChange} />
+                            <div className="error-field">{errorField.password && <p>password is required</p>}</div>
                         </div>
                     </div>
 
-                    <div className="form-row">
+                    <div className="form-row" /* confirm password */>
                         <div className="form-key">
                             <label htmlFor="">confirm password</label>
                         </div>
@@ -137,7 +221,7 @@ export function Signup({ closeButton }) {
                         </div>
                     </div>
 
-                    <div className=" row-signup">
+                    <div className=" row-signup" /* sign-up btn */>
                         <button>Signup</button>
                     </div>
                 </div>
